b51059e166455d5a21e9ef53ed585546d290a1ce
George Ailenei <george.ailenei@gmail.com>
2022-04-07 13:49:22 +0100

    LAB 2 - Reviewed and Completed

--- 01_Exercises/LAB_02.py
+++ 01_Exercises/LAB_02.py
@@ -3,45 +3,44 @@ import datetime
 
 def calculate_my_age(day, month, year):
     """
-    Input your date of birth and this function will return the number of days you lived so far.
-    :param day: must be an int depending on the month but not bigger than 31.
-    :param month: must be an int between 1 - 12
-    :param year: 1 - 9999
-    :return: int (your number of days lived on this planet)
-    day_in_each_month - the first item of the list is 0 days because month 0 does not exist.
+    Input: Day, Month and year - and this function will return how many days you lived on earth.
+    Example: 23, 12, 1992
     """
 
-    days_in_each_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_in_each_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
     current_year = datetime.date.today().year
-    days_in_a_year = 365
+    current_month = datetime.date.today().month
+    current_day = datetime.date.today().day
+    days_in_a_year = 365         # Every 4 years, a year has 366 days.
     years_lived = (current_year - 1) - year
-    days_lived_so_far = years_lived * days_in_a_year
-    days_this_year = 0
-    days_after_birth_month = 0
+    leap_year_days = int(years_lived / 4)   # Find how many additional days he/she lived.
+    days_lived_last_year = 0
+    days_lived_this_year = 0
 
-    while True:
+    if year <= 0:
+        return "Write your date of birth again"
 
-        if day > 31:
-            print("You must write the correct date")
-            break
-        elif month > 12:
-            print("You must write the correct date")
-            break
-        elif year > 10000 or year == 0:
-            print("You must write the correct date")
-            break
+    for days in range(month, len(days_in_each_month)):  # Days lived last year but not including the birthday month.
+        days_lived_last_year = days_lived_last_year + days_in_each_month[days]
 
-        for count_days in range(month, len(days_in_each_month) - month):
-            days_after_birth_month = days_after_birth_month + days_in_each_month[count_days]
-        days_after_birth_month = days_after_birth_month - (days_in_each_month[month] - day)
+    for days in range(current_month - 1):   # Days lived this year but not including this month.
+        days_lived_this_year = days_lived_this_year + days_in_each_month[days]
 
-        for count_days in range(month + 1):
-            days_this_year = days_this_year + days_in_each_month[count_days]
-        days_this_year = days_this_year + day
+    days_lived_after_last_birthday = (days_lived_this_year + days_lived_last_year) + (days_in_each_month[month - 1] - day) + current_day
+    total_days_lived = (days_in_a_year * years_lived) + days_lived_after_last_birthday + leap_year_days
 
-        total_days_lived = days_lived_so_far + days_after_birth_month + days_this_year
-        return total_days_lived
+    return total_days_lived
 
 
-print(calculate_my_age(8, 1, 1996))
+print(calculate_my_age(23, 4, 1992))
+print(calculate_my_age(23, 4, 0))
+print(calculate_my_age(23, 4, -1))
 
+
+assert calculate_my_age(23, 4, 1992) == 10941
+assert calculate_my_age(16, 12, 1989) == 11800
+assert calculate_my_age(6, 4, 2022) == 1
+assert calculate_my_age(5, 4, 2022) == 2
+assert calculate_my_age(11, 11, 1996) == 9278
+assert calculate_my_age(23, 4, -1) == "Write your date of birth again"
