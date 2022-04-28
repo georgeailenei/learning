1a651f79116c389427409c3bb5eacf27c60989a9
George Ailenei <george.ailenei@gmail.com>
2022-04-28 13:08:08 +0100

    Lab_03 done, it working the way you wanted to work

--- 01_Exercises/LAB_03.py
+++ 01_Exercises/LAB_03.py
@@ -73,103 +73,129 @@ def detect_the_longest_prime_nr_sequence(random_nr):
     Example: "22 1 3 11"
     :return: The longest sequence with prime numbers.
     """
-    # 1 3 5 22 10 3 5 11
-    # it must print 3 5 1 because is the longest sequence of prime numbers.
-    # - 1 2 - - 5 6 7
-    # a list with multiple lists where to store the sequences and then print the longest.
 
     list_of_integers = generate_int_list(random_nr)
     the_prime_numbers = list_the_prime_numbers(random_nr)
     prime_numbers_sequences = []
-    the_sequence = []
+
+    x = 0
     i = 0
     j = 0
 
     while i < len(list_of_integers):
-        if list_of_integers[i] == the_prime_numbers[j]:
-            the_sequence.append(list_of_integers[i])
+        if j == len(the_prime_numbers):
+            break
+        elif list_of_integers[i] == the_prime_numbers[j]:
+            if len(prime_numbers_sequences) == 0:
+                prime_numbers_sequences.append([])
+            prime_numbers_sequences[x].append(list_of_integers[i])
             i += 1
             j += 1
         elif list_of_integers[i] != the_prime_numbers[j]:
-            i += 1
+            if len(prime_numbers_sequences) == 0:
+                prime_numbers_sequences.append([])
+                i += 1
+            elif len(prime_numbers_sequences[x]) > 0:
+                prime_numbers_sequences.append([])
+                x += 1
+                i += 1
+            elif len(prime_numbers_sequences[x]) == 0:
+                i += 1
 
-    return prime_numbers_sequences
+    the_answer = []
+    if len(prime_numbers_sequences) > 1:
+        for sequence in prime_numbers_sequences:
+            for i in range(1, len(prime_numbers_sequences)):
+                if len(sequence) > len(prime_numbers_sequences[i]):
+                    the_answer.append(sequence)
+                elif len(sequence) < len(prime_numbers_sequences[i]):
+                    the_answer.clear()
+                    the_answer.append(prime_numbers_sequences[i])
+        return the_answer[0]
+    else:
+        return prime_numbers_sequences
 
 
-print(detect_the_longest_prime_nr_sequence("7 13 20 22 3 5"))
+assert detect_the_longest_prime_nr_sequence("1 2 11 3 10 20 40 3 5") == [2, 11, 3]
+assert detect_the_longest_prime_nr_sequence("22 1 3 11") == [[3, 11]]
+assert detect_the_longest_prime_nr_sequence("7 5 11 20 16 2 14 7 5 40 22 7 5 3 2") == [7, 5, 3, 2]
 
-#
-# def main_menu():
-#     """
-#     :return: A list of options/strings.
-#     """
-#     menu = ["Main Menu", "Option 1: To generate a list of integers input 1", "Option 2: To find prime numbers in a list input 2", "Option 3: To detect a mountain sequence number input 3"]
-#
-#     for options in menu:
-#         print(options)
-#
-#     return menu
-#
-#
-# def return_to_menu():
-#     """
-#     This function takes the user input to exit or to return to the main menu.
-#     """
-#     the_chosen_option = input("\n" "Write main menu to return to it or exit: ")
-#
-#     if the_chosen_option == "main menu":
-#         return main()
-#     else:
-#         print("You've exit the application")
-#         pass
-#
-#
-# def main():
-#     """
-#     This is the main function of the app which provides instructions and it takes a random number input.
-#     Examples:
-#         1213402
-#         -2-13-49 etc
-#     """
-#     main_menu()
-#     selected_option = input("Choose your option here: ")
-#
-#     if selected_option == "1":
-#         os.system("cls")
-#         print("You've choose option 1: Generate a list of integers")
-#         user_input = input("Write your numbers here: ")
-#         user_result = generate_int_list(user_input)
-#         print("Output:")
-#
-#         for num in user_result:
-#             print(num, end=" ")
-#
-#         return_to_menu()
-#
-#     elif selected_option == "2":
-#         os.system("cls")
-#         print("You've choose option 2: Find prime numbers")
-#         user_input = input("Write your numbers here: ")
-#         user_result = find_prime_numbers(user_input)
-#         print("Output:")
-#
-#         for num in user_result:
-#             print(num, end=" ")
-#
-#         return_to_menu()
-#
-#     elif selected_option == "3":
-#         os.system("cls")
-#         print("You've choose option 3: Detect a mountain sequence number")
-#         user_input = input("Write 5 numbers here: ")
-#         user_result = mountain_sequence_pattern(user_input)
-#         print("Result:")
-#         if user_result:
-#             print("The app detected a mountain sequence number")
-#         else:
-#             print("This number does NOT represent a mountain sequence number")
-#         return_to_menu()
-#
-#
-# if __name__ == "__main__":
-#     main()
+
+def main_menu():
+    """
+    :return: A list of options/strings.
+    """
+    menu = ["Main Menu", "Option 1: To generate a list of integers input 1", "Option 2: To find prime numbers in a list input 2", "Option 3: To detect the longest prime numbers sequence numbers, input 3"]
+
+    for options in menu:
+        print(options)
+
+    return menu
+
+
+def return_to_menu():
+    """
+    This function takes the user input to exit or to return to the main menu.
+    """
+    the_chosen_option = input("\n" "Write main menu to return to it or exit: ")
+
+    if the_chosen_option == "main menu":
+        return main()
+    else:
+        print("You've exit the application")
+        pass
+
+
+def main():
+    """
+    This is the main function of the app which provides instructions and it takes a random number input.
+    Examples:
+        1213402
+        -2-13-49 etc
+    """
+    main_menu()
+    selected_option = input("Choose your option here: ")
+
+    if selected_option == "1":
+        os.system("cls")
+        print("You've choose option 1: Generate a list of integers")
+        user_input = input("Write your numbers here: ")
+        user_result = generate_int_list(user_input)
+        print("Output:")
+
+        for num in user_result:
+            print(num, end=" ")
+
+        return_to_menu()
+
+    elif selected_option == "2":
+        os.system("cls")
+        print("You've choose option 2: Find prime numbers")
+        user_input = input("Write your numbers here: ")
+        user_result = list_the_prime_numbers(user_input)
+        print("Output:")
+
+        for num in user_result:
+            print(num, end=" ")
+
+        return_to_menu()
+
+    elif selected_option == "3":
+        os.system("cls")
+        print("You've choose option 3: Detect the longest prime numbers sequence")
+        user_input = input("Write some numbers here: ")
+        user_result = detect_the_longest_prime_nr_sequence(user_input)
+        print("The longest  sequence of prime numbers is:")
+
+        if len(user_result) == 1:
+            for num in range(len(user_result[0])):
+                print(user_result[0][num], end=" ")
+        else:
+            for num in user_result:
+                print(num, end=" ")
+
+        return_to_menu()
+
+
+if __name__ == "__main__":
+    main()
--- 01_Exercises/Tests.py
+++ 01_Exercises/Tests.py
@@ -1,6 +1,15 @@
 
+the_list = [1, 13, 22]
+the_answer = []
 
-a = [['alice'], ['bob'], ['cara']]
-b = []
+for num in the_list:
+    for i in range(1, len(the_list)):
+        if num > the_list[i]:
+            the_answer.append(num)
+        else:
+            the_answer.clear()
+            the_answer.append(the_list[i])
 
-b =
+print(the_answer)
+
+
