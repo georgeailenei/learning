8b9cbd00489e6e3e38bd6b7c5799bb110b58072a
George Ailenei <george.ailenei@gmail.com>
2022-05-14 11:35:34 +0100

    worked on removing expenses

--- 01_Exercises/Lab_4_to_6.py
+++ 01_Exercises/Lab_4_to_6.py
@@ -1,11 +1,9 @@
 """
 2. Ștergere.
-• Șterge toate cheltuielile pentru ziua dată
 • Șterge cheltuielile pentru un interval de timp (se dă ziua de început și
 sfârșit, se șterg toate cheltuielile pentru perioada dată)
 • Șterge toate cheltuielile de un anumit tip"""
 
-"""inainte de a face functiile de mai sus trebuie sa primeste datele corect. Corecteaza codul."""
 
 
 class Expense:
@@ -51,16 +49,31 @@ class Expense:
 
 def main_menu():    # information and options about the app.
     print("This App allows you to store your expenses.")
-    the_option = input("1. add expenses \n"
-                       "2. update expense \n"
-                       "3. exit \n"
+    the_option = input("1. Add expenses \n"
+                       "2. Update expenses \n"
+                       "3. Remove expenses \n"
+                       "4. Print full list of expenses \n"
+                       "5. Exit \n"
+                       "Please choose an option here: ")
+    return the_option
+
+
+def remove_menu():    # the Menu for remove option.
+    print("Choose from the options below")
+    the_option = input("1. Remove all expenses by date \n"
+                       "2. Remove all expenses by type \n"
+                       "3. Remove expenses by time \n"
+                       "4. Return to main menu \n"
                        "Please choose an option here: ")
     return the_option
 
 
 def exit_to_menu():
-    choose_option = input("Do you have more inputs to make? ")
-    return choose_option
+    choose_option = input("Do you want to continue? ")
+    if choose_option == "no":
+        return False
+    else:
+        return True
 
 
 def collect_data():     # collects the data and stores in as object.
@@ -77,42 +90,79 @@ def update_expense():   # collects another set of data for update.
 
 
 def main():     # the main func that puts everything together.
-    options = main_menu()
     all_expenses = []
 
-    if options == "1":      # add expenses
-        print("\nYou can now input your expenses")
+    while True:
+        options = main_menu()
 
-        while True:
-            data_collected = collect_data()
+        if options == "1":      # add expenses
+            print("\nYou can now input your expenses")
 
-            if data_collected.check_date() and data_collected.check_amount() and data_collected.check_type():
-                all_expenses.append(data_collected.get_all_expense_details())
+            while True:
+                data_collected = collect_data()
+                if data_collected.check_date() and data_collected.check_amount() and data_collected.check_type():
+                    all_expenses.append(data_collected.get_all_expense_details())
+                else:
+                    print("Please try to input your data again!")
+
+                print(all_expenses)     # display the current list of expenses.
+                if not exit_to_menu():
+                    break
+
+        elif options == "2":       # update expenses
+            update_the_expense = update_expense()
+
+            if update_the_expense in all_expenses:
+                all_expenses.remove(update_the_expense)
+                print("Insert a new expense to update")
+                update_the_expense = update_expense()
+                all_expenses.append(update_the_expense)
             else:
-                print("Please try to input your data again!")
+                print("We could not find your expense, try again!")
 
-            print(all_expenses)     # display the current list of expenses.
-
-            if exit_to_menu() == "no":
-                break
-            else:
+            if not exit_to_menu():
                 continue
 
-    elif options == "2":       # update expenses
-        update_the_expense = update_expense()
+        elif options == "3":
+            other_options = remove_menu()
+            while True:
+                if other_options == "1":
+                    print("\nYou can now remove and expense by inserting the Date")
+                    the_date_of_expense = input("Date (dd-mm-yy): ")
 
-        if update_the_expense in all_expenses:
-            all_expenses.remove(update_the_expense)
-            print("Please update insert a expense to update")
-            update_the_expense = update_expense()
-            all_expenses.append(update_the_expense)
+                    for expense in all_expenses:
+                        if the_date_of_expense not in expense:
+                            print(all_expenses)
+                            f"The {expense} has been removed!"
+                            all_expenses.remove(expense)
+                            print("Your new expenses list is:")
+                            print(all_expenses)
 
-        else:
-            print("We could not find your expense, try again!")
+                    if not exit_to_menu():
+                        continue
 
-    elif options == "3":       # exit app
-        if len(all_expenses) == 0:
-            print("You really didn't spend any money? ok. goodbye!")
+                elif other_options == "2":
+                    pass
+
+                elif other_options == "3":
+                    pass
+
+                elif other_options == "4":
+                    break
+
+        elif options == "4":
+            print("\nThis is your list of expenses:")
+            print(all_expenses)
+
+            if not exit_to_menu():
+                continue
+
+        elif options == "5":       # exit app
+            if len(all_expenses) == 0:
+                print("You really didn't spend any money? ok. goodbye!")
+                break
+            else:
+                break
 
 
 if __name__ == '__main__':
